4897b1cc05a626f1c3352a051bc4f4f5e8e06a31
Quido Meijer <qmeijer@bol.com>
2022-12-03 14:49:11 +0100

    Clean up part 1 a bit

--- src/main/kotlin/me/quido/year2022/day01/Day01.kt
+++ src/main/kotlin/me/quido/year2022/day01/Day01.kt
@@ -5,18 +5,16 @@ import me.quido.util.getInputAsText
 fun main() {
     val input = getInputAsText(2022, 1) ?: error("Input file does not exist")
 
-    val data = input.split("\n\n").map {
-            elf -> elf.lines().map { it.toInt()}
-    }
-    println(data.maxOf {it.sum()})
-//    println(Day01().getHighestCalorieCountFromElves(input))
+    println(Day01().getHighestCalorieCountFromElves(input))
 }
+
 
 class Day01 {
     fun getHighestCalorieCountFromElves(input: String): Int {
-        val data = input.split("\n\n").map {
-            elf -> elf.lines().map { it.toInt()}
+        val data = input.split("\n\n").map { elf ->
+            elf.lines().map { it.toInt() }
         }
-        return data.maxOf { it.sum()}
+
+        return data.maxOf { it.sum() }
     }
 }
