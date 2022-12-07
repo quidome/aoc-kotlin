f48112d0957aa421d09bf72d7ef9c02de430c3a3
Quido Meijer <qmeijer@bol.com>
2022-12-07 14:15:14 +0100

    Working solution for day 2, part 1

---
+++ src/test/kotlin/me/quido/year2022/day02/Day02Test.kt
@@ -0,0 +1,33 @@
+package me.quido.year2022.day02
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+class Day02Test {
+    private val input = """
+        A Y
+        B X
+        C Z
+        """.trimIndent().trimEnd()
+
+    private val solver = Day02(input)
+
+    @Test
+    fun givenTestSetMethodShouldReturn15() {
+        val expected = 15
+
+        val output = solver.rockPaperScissorsScore()
+
+        assertEquals(expected, output)
+    }
+
+    @Test
+    fun givenTestSetMethodShouldReturn12() {
+        val expected = 12
+
+        val output = solver.rockPaperScissorsScoreNextGen()
+
+        assertEquals(expected, output)
+    }
+}
